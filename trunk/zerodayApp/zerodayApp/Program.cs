7841a38c8d123be8fa6eea2035be1b6c5d4361f0
yul85yr@gmail.com <yul85yr@gmail.com@cdfa1c9b-3f4c-898a-c341-a26bb566276e>
2013-05-24 03:30:18 +0000

    StartEncoding 버튼 입력시에 입력한 text를 is.exe를 실행해서 encoding하도록 수정

--- trunk/zerodayApp/zerodayApp/Program.cs
+++ trunk/zerodayApp/zerodayApp/Program.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
+using zerodayApp;
+
 namespace zerodayApp
 {
+
+
     static class Program
     {
         /// <summary>
@@ -15,7 +19,14 @@ namespace zerodayApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new Form1());
+            
+
         }
+
     }
+
+    
+
 }
