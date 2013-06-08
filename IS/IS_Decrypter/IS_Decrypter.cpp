becab38fba2c7f9a68d50247ecdce3c1e1238b7a
manguluv@gmail.com <manguluv@gmail.com@cdfa1c9b-3f4c-898a-c341-a26bb566276e>
2013-06-08 15:03:57 +0000

    Add comment for korean. Write what to do.

--- IS/IS_Decrypter/IS_Decrypter.cpp
+++ IS/IS_Decrypter/IS_Decrypter.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <string>
 #include "midi.h"
+#include "../IS/MPUINT.H"
 
 
 int main(int argc, char *argv[]) 
@@ -12,6 +13,15 @@ int main(int argc, char *argv[])
     midi decode; 
       
     decode.parsexml(file);
+	/*현재 process 부분에서 전체 메세지를 따로 나누어 하나의 char를 다루는 것처럼 취급하고있다.
+	process 함수를 호출하지 않고 각각의 note를 그냥 int형 배열에 넣으면 encoder 파트에서 midi file에
+	입력하기 직전의 int*를 얻을 수 있다.
+	현재 encoder에서 rsa 암호화 부분은 복호화 기능이 전부 구현되어 있음으로 #include로 RSAEncrypter class만 참조하면
+	바로 사용 가능하다.
+	문제는 WordtoInt class에서 GetMpuint, Converter method인데 이것을 반대로 적용하는 부분은 구현되어 있진 않다.
+	100단위, 1000단위로 데이터를 끊어서 처리하는 부분이므로 이부분만 적당히 거꾸로 구현하면 될듯
+*/
+
 	decode.process();
     decode.results(); 
 
